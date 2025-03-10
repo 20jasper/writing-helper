1d45331a9cd06d63fc9b73d3bdb92c9635e00794
Jacob Asper <jacobasper191@gmail.com>
2025-03-10 19:14:59 -0400

    fix eslint solid

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,21 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import solid from "eslint-plugin-solid/configs/recommended";
 import "eslint-plugin-only-warn";
+import * as tsParser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"], ...solid },
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...solid,
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
